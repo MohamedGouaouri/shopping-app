c4f8dbc9cb67b0c986134dd5df4da7b454cba2a3
MohamedGouaouri <im_gouaouri@esi.dz>
2021-08-06 16:29:23 +0100

    fix product count state

--- lib/views/cart.dart
+++ lib/views/cart.dart
@@ -9,98 +9,97 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  List<Product> products = [
+    Product(
+        id: 1,
+        name: "A",
+        price: 200.0,
+        description: "AAAAAAA",
+        rate: 4.4,
+        number: 1),
+    Product(
+        id: 1,
+        name: "B",
+        price: 200.0,
+        description: "AAAAAAA",
+        rate: 4.4,
+        number: 1),
+  ];
+
+  _buildCartProductsItem(double _width) {
+    List<Widget> widgets = [];
+    for (var p in products) {
+      widgets.add(
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                )
+              ]),
+          child: Row(
+            children: [
+              Container(
+                width: _width / 6,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.delete,
+                  ),
+                ),
+              ),
+              Container(
+                width: 2 * _width / 3,
+                child: ListTile(
+                  leading: Text("Image"),
+                  title: Text(p.name),
+                  subtitle: Text("\$${p.price}"),
+                ),
+              ),
+              Container(
+                width: _width / 6,
+                child: Column(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          p.number = p.number + 1;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.add,
+                      ),
+                    ),
+                    Text("${p.number}"),
+                    IconButton(
+                        onPressed: () {
+                          if (p.number >= 1) {
+                            setState(() {
+                              p.number = p.number - 1;
+                            });
+                          }
+                        },
+                        icon: Icon(Icons.remove))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+    return widgets;
+  }
+
   @override
   Widget build(BuildContext context) {
     final _width = MediaQuery.of(context).size.width;
-    List<Product> products = [
-      Product(
-          id: 1,
-          name: "A",
-          price: 200.0,
-          description: "AAAAAAA",
-          rate: 4.4,
-          number: 1),
-      Product(
-          id: 1,
-          name: "B",
-          price: 200.0,
-          description: "AAAAAAA",
-          rate: 4.4,
-          number: 1),
-    ];
-
-    _buildCartProductsItem() {
-      List<Widget> widgets = [];
-      for (var p in products) {
-        widgets.add(
-          Container(
-            margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3),
-                  )
-                ]),
-            child: Row(
-              children: [
-                Container(
-                  width: _width / 6,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.delete,
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 2 * _width / 3,
-                  child: ListTile(
-                    leading: Text("Image"),
-                    title: Text(p.name),
-                    subtitle: Text("\$${p.price}"),
-                  ),
-                ),
-                Container(
-                  width: _width / 6,
-                  child: Column(
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            p.number = p.number + 1;
-                          });
-                          print(p.number);
-                        },
-                        icon: Icon(
-                          Icons.add,
-                        ),
-                      ),
-                      Text("${p.number}"),
-                      IconButton(
-                          onPressed: () {
-                            if (p.number >= 1) {
-                              setState(() {
-                                p.number = p.number - 1;
-                              });
-                            }
-                          },
-                          icon: Icon(Icons.remove))
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        );
-      }
-      return widgets;
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Your cart"),
@@ -108,7 +107,7 @@ class _CartPageState extends State<CartPage> {
         actions: [],
       ),
       body: ListView(
-        children: _buildCartProductsItem(),
+        children: _buildCartProductsItem(_width),
       ),
     );
   }
